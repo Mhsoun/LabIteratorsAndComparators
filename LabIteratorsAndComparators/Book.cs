05ca54301b2d2be09c3ec4f7b05f701e3973ce9e
Mohammad Mhsoon <mmhsoon@hotmail.com>
2020-05-26 11:10:09 +0200

    Add project files.

---
+++ LabIteratorsAndComparators/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LabIteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors;
+        }
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public IReadOnlyList<string> Authors { get; set; }
+
+        public int CompareTo(Book other)
+        {
+            int result = this.Year.CompareTo(other.Year);
+            if (result==0)
+            {
+                result = this.Title.CompareTo(other.Title);
+            }
+            return result;
+
+        }
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+    }
+}
---
+++ LabIteratorsAndComparators/Library.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LabIteratorsAndComparators
+{
+    public class Library : IEnumerable
+    {
+        internal List<Book> Books { get; set; }
+
+        public Library (params Book[] books)
+        {
+            Books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.Books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+            private int currentIndx;
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                this.Reset();
+                this.books = new List<Book>(books);
+            }
+
+            public void Dispose(){ }
+            public bool MoveNext() => ++this.currentIndx < this.books.Count;
+            public void Reset() => this.currentIndx = -1;
+            public Book Current => this.books[this.currentIndx];
+            object IEnumerator.Current => this.Current;
+            
+        }
+    }
+}
